cb4c18cd01bc09205e1c03c76b68da2b9c939db1
Nathanael Gentry <ngentry1@liberty.edu>
2019-09-28 11:16:21 -0400

    make credential dataclass

--- timeshaver.py
+++ timeshaver.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from dataclasses import dataclass
 
 ***REMOVED***
 
@@ -21,33 +22,30 @@ class AuthenticationError(AdpError):
     message = "The user attempted an unauthorized access to a resource."
 
 
-class TimeSaver():
+@dataclass
+class Credentials:
+    uid: str
+    passwd: str
+    
+class TimeSaver:
     def __init__(self):
         # Create a headless browser
         options = Options()
         self.driver = webdriver.Firefox(options=options)
         self.driver.get(TIMESAVER_FRONTPAGE)
-        self.cred = {"uid": None, "paswd": None}
+        self.credentials = None
 
     def __del__(self):
         # Delete the headless browser
         self.driver.close()
-        
-    @property
-    def credentials(self):
-        return self.cred
-
-    @credentials.setter
-    def credentials(uid, passwd):
-        self.cred = Credentials(uid, passwd)
 
     def authenticate(self):
         """Login to TimeSaver with the provided credentials."""
         uid_field = self.driver.find_element_by_id("username")
         passwd_field = self.driver.find_element_by_id("password")
 
-        uid_field.send_keys(self.cred.uid)
-        passwd_field.send_keys(self.cred.passwd)
+        uid_field.send_keys(self.credentials.uid)
+        passwd_field.send_keys(self.credentials.passwd)
 
         self.driver.find_element_by_id("bttSubmit").click()
         if not self.is_authenticated():
